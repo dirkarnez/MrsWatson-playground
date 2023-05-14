50c6b1ae2601906a38825ac75a3ab8569081c69a
Dirk Arnez <smalldirkalex@gmail.com>
2023-05-15 00:06:17 +0800

    - init, working

---
+++ run.cmd
@@ -0,0 +1,3 @@
+mrswatson64.exe --plugin-root "%USERPROFILE%\Downloads\install_tal-chorus-lx" --list-plugins --verbose
+mrswatson64.exe --plugin-root "%USERPROFILE%\Downloads\install_tal-chorus-lx" --input 2022polina.wav --output 2022polina_out.wav --plugin "TAL-Chorus-LX-64"
+pause
